22faa1b30913342c50f01c566fab32016add9999
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2021-04-13 16:26:18 -0700

    created redux state actions

---
+++ src/redux/actions.js
@@ -0,0 +1,79 @@
+import firebase from "../firebase/firebase";
+
+export function createEmailAccount(email, password) {
+  return async (dispatch) => {
+    try {
+      console.log("create email function");
+      const user = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      console.log("after user");
+      dispatch(loggedIn(user, true));
+    } catch (error) {
+      console.log("sign up error ", error);
+      dispatch(registerError(error.message));
+    }
+  };
+}
+
+export function loginEmailAccount(email, password) {
+  return async (dispatch) => {
+    try {
+      const user = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+      dispatch(loggedIn(user));
+      console.log(user)
+    } catch (error) {
+        console.log(error)
+      dispatch(loginError(error.message));
+    }
+  };
+}
+
+export function logout() {
+  return async (dispatch) => {
+    try {
+      await firebase.auth().signOut();
+      dispatch(loggedOut());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+function loggedIn(user, isNew=false) {
+  return {
+    type: "LOGGED_IN",
+    payload: {
+        user, 
+        newUser: isNew
+    },
+  };
+}
+function loggedOut(user) {
+  return {
+    type: "LOGGED_OUT",
+  };
+}
+
+export function registerError(error) {
+  return {
+    type: "REGISTER_ERROR",
+    payload: error,
+  };
+}
+
+export function loginError(error) {
+  return {
+    type: "LOGIN_ERROR",
+    payload: error,
+  };
+}
+
+export function uploadBusiness(details) {
+    return {
+        type: "UPLOAD_BUSINESS",
+        payload: details
+    }
+}
