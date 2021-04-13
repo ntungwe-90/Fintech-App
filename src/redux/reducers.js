b25edb674a855665688e28036fefb6105d13add0
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2021-04-13 16:29:42 -0700

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,44 +1,56 @@
 // import {v4 as uuid} from 'uuid';
 
 const initialState = {
-  users: [],
+  users: {},
   loggedIn: false,
+  error: {},
+  newUser: false,
+  buisness: new Array()
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
+const reducer = (state = initialState, {type, payload}) => {
+  switch (type) {
     case "ADD_USER":
       const newUser = {
         // id: uuid(),
-        name: action.payload.name,
-        email: action.payload.email,
-        tel: action.payload.tel,
+        name: payload.name,
+        email: payload.email,
+        tel: payload.tel,
       };
-      return { 
-        ...state, 
-        users: [...state.users, newUser],
-        loggedIn: true
-      };
-    case "LOGIN_USER":
       return {
+        ...state,
+        users: [...state.users, newUser],
         loggedIn: true,
-      }
+      };
+    case "LOGGED_IN":
+      return {
+        ...state,
+        loggedIn: true,
+        ...payload,
+      };
     case "DELETE_USER":
       const FilteredUsers = state.users.filter(
-        (user) => user.id !== action.payload
+        (user) => user.id !== payload
       );
       return { ...state, users: FilteredUsers };
     case "EDIT_USER":
       const editedUsers = state.users.map((user) => {
-        if (user.id === action.user_id) {
-          return { ...user, ...action.updated_info };
+        if (user.id === user_id) {
+          return { ...user, ...updated_info };
         } else {
           return user;
         }
       });
       return { ...state, users: editedUsers };
     case "SET_ALL_USERS":
-      return { users: action.payload };
+      return { users: payload };
+    case "UPLOAD_BUSINESS":
+      // const {buisness} = state;
+      // buisness.push(payload)
+      return {
+        ...state,
+        buisness: [...(state.buisness || []), payload]
+      }
     default:
       return state;
   }
