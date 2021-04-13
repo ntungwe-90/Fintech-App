105081f9d749774f853e2df85b8a1afca3b9d9b6
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2021-04-13 16:30:43 -0700

    Update AllBuisness.js

--- src/screens/AllBuisness.js
+++ src/screens/AllBuisness.js
@@ -1,57 +1,100 @@
 import React from "react";
 import { FlatList, scrowView, Text, View, StyleSheet, Image } from "react-native";
+import { connect } from "react-redux"
 import Buisness from './buisness';
+// import {buisness} from "../dummy-data"
 
-export default function AllBuisness({navigation}) {
-  const buisness = [
-    {
-      image: require("../../assets/saloon.jpg"), 
-      name: "SALON",
-      products: "manicure,pendicure massage",
-      description: "WE OFFER MANICURES, PENDICURES, MASSAGES AND MOBILE MONEY",
-      Rating: "34",
-      rate: "rate",
-      photos: [
-        require("../../assets/m2.jpg"),
-        require("../../assets/s1.jpg"),
-        require("../../assets/makeup.jpg"),
-        require("../../assets/polish.jpg"),
-        require("../../assets/makeup2.jpg"),
-        require("../../assets/massage.jpg"),
-      ],
-      moreImages: [
-        require("../../assets/m2.jpg"),
-        require("../../assets/saloon2.jpg"),
-      ],
-      contact: {
-        location: "Abeka, Circle, Achimota.",
-        tel: "0259334563",
-        email: "saloonD@gmail.com",
-      },
-      capital: 100000,
+const buisness = [
+  {
+    image: require("../../assets/saloon.jpg"),
+    name: "SALON",
+    products: "manicure,pendicure massage",
+    description: "WE OFFER MANICURES, PENDICURES, MASSAGES AND MOBILE MONEY",
+    Rating: "34",
+    rate: "rate",
+    photos: [
+      require("../../assets/m2.jpg"),
+      require("../../assets/s1.jpg"),
+      require("../../assets/makeup.jpg"),
+      require("../../assets/polish.jpg"),
+      require("../../assets/makeup2.jpg"),
+      require("../../assets/massage.jpg"),
+    ],
+    moreImages: [
+      require("../../assets/m2.jpg"),
+      require("../../assets/saloon2.jpg"),
+    ],
+    contact: {
+      location: "Abeka, Circle, Achimota.",
+      tel: "0259334563",
+      email: "saloonD@gmail.com",
     },
-    {
-      image: require("../../assets/cafe.jpg"),   
-      name: "Restaurant",
-      products: "drinks food,dance",
-      Rating: "34",
-      rate: "rate",
+    capital: 100000,
+  },
+  {
+    image: require("../../assets/cafe.jpg"),
+    name: "Restaurant",
+    products: " food, drinks, dance",
+    description: "WE Specialise in both continental and traditional meals",
+    Rating: "34",
+    rate: "rate",
+
+    photos: [
+      require("../../assets/burgers1.jpg"),
+      require("../../assets/friedrice.jpg"),
+      require("../../assets/fruits.jpg"),
+      require("../../assets/paster1.jpg"),
+      require("../../assets/salad.jpg"),
+      require("../../assets/mixed.jpg"),
+    ],
+    moreImages: [
+      require("../../assets/burgers1.jpg"),
+      require("../../assets/cheese.jpg"),
+    ],
+    contact: {
+      location: "lagon, kumasi, redcomadina.",
+      tel: "0257423843",
+      email: "restuaflash@gmail.com",
     },
-    {
-      image: require("../../assets/casino.jpg"),
-      name: "Casino",
-      products: "poker, games, drinks",
-      Rating: "34",
-      rate: "rate",
+    capital: 5000,
+  },
+  {
+    image: require("../../assets/casino2.jpg"),
+    name: "Hotel",
+    products: "kitchen, rooms, drinks",
+    Rating: "34",
+    rate: "rate",
+
+    photos: [
+      require("../../assets/bed.jpg"),
+      require("../../assets/hotel.jpg"),
+      require("../../assets/hotel2.jpg"),
+      require("../../assets/casino.jpg"),
+      require("../../assets/casino2.jpg"),
+      require("../../assets/mixed.jpg"),
+    ],
+    moreImages: [
+      require("../../assets/h1.jpg"),
+      require("../../assets/reception.jpg"),
+    ],
+    contact: {
+      location: "achimota, kozoa, madina.",
+      tel: "05944329",
+      email: "hotelez@gmail.com",
     },
-    {
-      image: require("../../assets/s1.jpg"),
-      name: "Electronics",
-      products: "Laptops, fridges, Irons",
-      rating: "34",
-      rate: "rate",
-    },
-  ];
+    capital: 5000,
+  },
+  {
+    image: require("../../assets/s1.jpg"),
+    name: "Electronics",
+    products: "Laptops, fridges, Irons",
+    rating: "34",
+    rate: "rate",
+  },
+];
+
+
+function AllBuisness({navigation, ...props}) {
   return (
     <View style={styles.container}>
       <View style={styles.image_conainer}>
@@ -59,7 +102,7 @@ export default function AllBuisness({navigation}) {
       </View>
       <View style={styles.flatlist}>
       <FlatList
-         data={buisness}
+         data={[...buisness, ...props.buisness]}
          renderItem={({ item }) => {
            return (
              <Buisness
@@ -68,7 +111,7 @@ export default function AllBuisness({navigation}) {
              />
            );
          }}
-         keyExtractor={(item, index) => `${item.name}-${index}`}
+         keyExtractor={(item, index) => `${item?.name}-${index}`}
          />
       </View>
     </View>
@@ -97,3 +140,5 @@ const styles = StyleSheet.create({
     flex: 8,
   }
 })
+
+export default connect(state => ({buisness: state.buisness}))(AllBuisness)
