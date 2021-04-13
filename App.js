3fe87c246d3194e0c950b23e616a76e4981768c4
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2021-04-13 16:30:12 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Navigator from './src/Navigator';
-import { connect, Provider } from "react-redux";
-import store from "./src/redux/store";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
 
 export default function App(props) {
   return (
