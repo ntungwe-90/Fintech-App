dec12e43fe3e55a79267ae7bac825b4a89079f52
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2021-04-13 16:32:17 -0700

    Create dummy-data.js

---
+++ src/dummy-data.js
@@ -0,0 +1,88 @@
+export const buisness = [
+  {
+    image: require("../assets/saloon.jpg"),
+    name: "SALON",
+    products: "manicure,pendicure massage",
+    description: "WE OFFER MANICURES, PENDICURES, MASSAGES AND MOBILE MONEY",
+    Rating: "34",
+    rate: "rate",
+    photos: [
+      require("../assets/m2.jpg"),
+      require("../assets/s1.jpg"),
+      require("../assets/makeup.jpg"),
+      require("../assets/polish.jpg"),
+      require("../assets/makeup2.jpg"),
+      require("../assets/massage.jpg"),
+    ],
+    moreImages: [
+      require("../assets/m2.jpg"),
+      require("../assets/saloon2.jpg"),
+    ],
+    contact: {
+      location: "Abeka, Circle, Achimota.",
+      tel: "0259334563",
+      email: "saloonD@gmail.com",
+    },
+    capital: 100000,
+  },
+  {
+    image: require("../assets/cafe.jpg"),
+    name: "Restaurant",
+    products: " food, drinks, dance",
+    description: "WE Specialise in both continental and traditional meals",
+    Rating: "34",
+    rate: "rate",
+
+    photos: [
+      require("../assets/burgers1.jpg"),
+      require("../assets/friedrice.jpg"),
+      require("../assets/fruits.jpg"),
+      require("../assets/paster1.jpg"),
+      require("../assets/salad.jpg"),
+      require("../assets/mixed.jpg"),
+    ],
+    moreImages: [
+      require("../assets/burgers1.jpg"),
+      require("../assets/cheese.jpg"),
+    ],
+    contact: {
+      location: "lagon, kumasi, redcomadina.",
+      tel: "0257423843",
+      email: "restuaflash@gmail.com",
+    },
+    capital: 5000,
+  },
+  {
+    image: require("../assets/casino2.jpg"),
+    name: "Hotel",
+    products: "kitchen, rooms, drinks",
+    Rating: "34",
+    rate: "rate",
+
+    photos: [
+      require("../assets/bed.jpg"),
+      require("../assets/hotel.jpg"),
+      require("../assets/hotel2.jpg"),
+      require("../assets/casino.jpg"),
+      require("../assets/casino2.jpg"),
+      require("../assets/mixed.jpg"),
+    ],
+    moreImages: [
+      require("../assets/h1.jpg"),
+      require("../assets/reception.jpg"),
+    ],
+    contact: {
+      location: "achimota, kozoa, madina.",
+      tel: "05944329",
+      email: "hotelez@gmail.com",
+    },
+    capital: 5000,
+  },
+  {
+    image: require("../assets/s1.jpg"),
+    name: "Electronics",
+    products: "Laptops, fridges, Irons",
+    rating: "34",
+    rate: "rate",
+  },
+];
